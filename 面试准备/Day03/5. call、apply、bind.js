718f4c41a2413565f74bfd95927f57c8b8e07fa2
hqchqc <734986933@qq.com>
2023-04-22 21:22:37 +0800

    feat: call bind apply

---
+++ 面试准备/Day03/5. call、apply、bind.js
@@ -0,0 +1,56 @@
+Function.prototype.myCall = function (context) {
+  if (typeof this !== "function") {
+    console.error("type error");
+  }
+
+  let args = [...arguments].slice(1),
+    result = null;
+
+  context = context || window;
+
+  context.fn = this;
+
+  result = context.fn(...args);
+
+  delete context.fn;
+
+  return result;
+};
+
+Function.prototype.myApply = function (context) {
+  if (typeof this !== "function") {
+    console.error("type error");
+  }
+
+  let result = null;
+
+  context = context || window;
+
+  context.fn = this;
+
+  if (arguments[1]) {
+    result = context.fn(...arguments[1]);
+  } else {
+    result = context.fn();
+  }
+
+  delete context.fn;
+
+  return result;
+};
+
+Function.prototype.myBind = function (context) {
+  if (typeof this !== "function") {
+    console.error("type error");
+  }
+
+  let args = [...arguments].slice(1),
+    fn = this;
+
+  return function Fn() {
+    return fn.apply(
+      this instanceof Fn ? this : context,
+      args.concat(...arguments)
+    );
+  };
+};
